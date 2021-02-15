89f29e18cfe9873d08a7290e7c38b05efdbb6e7b
Muhammad Usama Bin Akhtar Khan <25438061+usamakhangt4@users.noreply.github.com>
2021-02-15 10:26:06 +0500

    made navbar component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,16 @@
+import { Navbar } from "../components";
+
 export default function Home() {
   return (
-    <main className="flex items-center justify-center w-full h-screen bg-purple-600">
-      <h1 className=" mx-1 text-6xl text-white">
-        React App boilerplate with typescript and tailwind
-      </h1>
-    </main>
+    <>
+      <header>
+        <Navbar />
+      </header>
+      <main className="flex items-center justify-center w-full h-screen bg-purple-600">
+        <h1 className=" mx-1 text-6xl text-white">
+          React App boilerplate with typescript and tailwind
+        </h1>
+      </main>
+    </>
   );
 }
