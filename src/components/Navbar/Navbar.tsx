d0f89359a928615c985d3d5c492ca8523951a80b
usamakhan019 <usama.khan@emumba.com>
2021-02-25 18:09:46 +0500

    navbar updated

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+import { CloseIcon, MenuIcon } from "svgs";
+import { classNames } from "utils/helper";
+
+export default function Navbar() {
+  const [navOpen, setNavOpen] = useState(true);
+
+  const mobilenavClasses = classNames({
+    "md:flex flex-col items-center justify-center gap-2 md:gap-5  md:pr-3 bg-white md:shadow-none shadow-md md:flex-row": true,
+    hidden: !navOpen,
+    flex: navOpen,
+  });
+  return (
+    <header className="font-nunito py-5">
+      <nav className="max-w-7xl md:flex-row md:items-center md:justify-between flex flex-col mx-auto">
+        <section className=" flex items-center justify-between px-3">
+          <img src="/images/mf-logo.svg" alt="logo" className="h-14" />
+          <button
+            onClick={() => setNavOpen(!navOpen)}
+            className="sm:block md:hidden text-purple-800">
+            {!navOpen ? MenuIcon : CloseIcon}
+          </button>
+        </section>
+        <section className={mobilenavClasses}>
+          <a
+            href="/"
+            className="hover:text-purple-600 text-lg font-light text-black cursor-pointer">
+            Mentorship
+          </a>
+          <button className="rounded-3xl hover:bg-purple-600 hover:text-white px-3 py-2 text-lg text-purple-600 transition-all duration-300 ease-in-out border-2 border-purple-600">
+            Say Hello
+          </button>
+        </section>
+      </nav>
+    </header>
+  );
+}
