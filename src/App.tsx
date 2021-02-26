b1024c3dbcee28a32263f2273606c76cca8c413b
Muhammad Usama Bin Akhtar Khan <25438061+usamakhangt4@users.noreply.github.com>
2021-02-26 13:11:27 +0500

    updated navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { Navbar } from "components/Navbar";
-import { Home } from "pages";
 
 export default function App() {
   return (
     <>
       <Navbar />
-      <Home />
     </>
   );
 }
