3ae979f9e7effdb58843b5a9d9bcbba19ad1ad3d
usamakhan019 <usama.khan@emumba.com>
2021-04-30 15:02:18 +0500

    started working on reacent work section

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -2,7 +2,7 @@ import { heroData } from "json_objects";
 import { PersonIcon, HeroIcon } from "svgs";
 export default function Hero() {
   return (
-    <main className="font-nunito flex flex-col items-center justify-center px-6 py-12">
+    <main className="font-nunito flex flex-col items-center justify-center px-6 pt-24">
       <h1 className="mb-6 text-5xl font-extrabold text-gray-800">
         {heroData.heading}
       </h1>
@@ -10,7 +10,7 @@ export default function Hero() {
         {heroData.description}
       </p>
       <figure className="my-8">{PersonIcon}</figure>
-      <figure className="">{HeroIcon}</figure>
+      <figure className="mt-16">{HeroIcon}</figure>
     </main>
   );
 }
--- src/components/Intro/index.tsx
+++ src/components/Intro/index.tsx
@@ -3,7 +3,7 @@ import { AboutMe } from "components";
 
 export default function index() {
   return (
-    <main className=" font-nunito relative flex flex-col items-center w-full h-screen">
+    <main className=" font-nunito flex flex-col items-center w-full">
       <section className="pt-36 pb-72 flex flex-col items-center justify-center w-full text-white bg-purple-800">
         <h1 className="mb-6 text-3xl font-bold">{intro.heading}</h1>
         <p className="line max-w-3xl text-xl font-light leading-normal text-center">
@@ -11,7 +11,7 @@ export default function index() {
         </p>
       </section>
 
-      <section className="top-2/3 max-w-7xl absolute left-auto">
+      <section className="max-w-7xl left-auto -mt-32">
         <AboutMe />
       </section>
     </main>
