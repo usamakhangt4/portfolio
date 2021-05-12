6b3783f3844236b2c5681f6e67f7bb6a4b5ba857
usamakhan019 <usama.khan@emumba.com>
2021-05-12 05:16:21 +0500

    completed recent work section

--- src/components/RecentWork/index.tsx
+++ src/components/RecentWork/index.tsx
@@ -1,7 +1,8 @@
 import { recentWorkList } from "json_objects";
 
 export default function index() {
-  const { title, description, mail, list, seeMore } = recentWorkList;
+  const { title, description, mail, list, seeMore, seeMoreIcon, iconWithLink } =
+    recentWorkList;
 
   return (
     <main className="py-36 font-nunito grid items-center justify-center px-6 text-center">
@@ -15,12 +16,30 @@ export default function index() {
 
       <section className="grid grid-cols-3 gap-6 py-16">
         {list.map((work) => (
-          <article className="w-96 h-72 border">card</article>
+          <section className="group w-96 h-72 rounded-xl relative border">
+            <article className="group-hover:bg-indigo-900 gap-7 rounded-xl group-hover:opacity-100 absolute inset-0 grid items-center content-center transition-all duration-300 ease-in-out opacity-0">
+              <p className="font-Exo_2 max-w-xs m-auto text-2xl text-white">
+                {work.title}
+              </p>
+              <a
+                href={work.link}
+                className="hover:bg-purple-600 hover:text-white h-11 w-max flex items-center justify-center gap-2 px-4 py-2 mx-auto text-lg text-purple-600 transition-all duration-300 ease-in-out border-2 border-purple-600 rounded-full">
+                <span>Visit Website</span>
+                <figure className="w-3">{iconWithLink}</figure>
+              </a>
+            </article>
+            <img
+              src={work.img}
+              alt="pic of recent work"
+              className="rounded-xl"
+            />
+          </section>
         ))}
       </section>
 
-      <button className="rounded-3xl hover:bg-purple-600 hover:text-white px-3 py-2 text-lg text-purple-600 transition-all duration-300 ease-in-out border-2 border-purple-600">
-        See more on Dribbble
+      <button className="hover:bg-purple-600 hover:text-white flex items-center gap-2 px-4 py-2 m-auto text-xl text-purple-600 transition-all duration-300 ease-in-out border-2 border-purple-600 rounded-full">
+        <figure className="w-5">{seeMoreIcon}</figure>
+        <span>{seeMore}</span>
       </button>
     </main>
   );
