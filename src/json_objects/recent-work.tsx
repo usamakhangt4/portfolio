c55af3773403eb3195fd14db9de9dc504b905865
Muhammad Usama Bin Akhtar Khan <25438061+usamakhangt4@users.noreply.github.com>
2021-05-12 05:16:21 +0500

    completed recent work section

--- src/json_objects/recent-work.tsx
+++ src/json_objects/recent-work.tsx
@@ -1,39 +1,43 @@
+import { angleRightIcon, DribbleIcon } from "svgs";
+
 export const recentWorkList = {
   title: "My Recent Work",
   description:
     "Here are a few design projects I've worked on recently. Want to see more?",
-  seeMore: "",
+  seeMore: "See more on dribble",
+  seeMoreIcon: DribbleIcon,
   mail: "mailto:usamakhangt4@gmail.com",
+  iconWithLink: angleRightIcon,
   list: [
     {
       title:
         "High-end, custom residential renovaters serving Fraser Valley homeowners.",
       link: "/",
-      img: "",
+      img: "https://source.unsplash.com/random/384x288?software",
     },
     {
       title:
         "Uniting First Nations culture and community through winter sports competition.",
       link: "/",
-      img: "",
+      img: "https://source.unsplash.com/random/384x288?software",
     },
     {
       title:
         "Blockchain pioneers bringing transparency, trust, and security to supply.",
       link: "/",
-      img: "",
+      img: "https://source.unsplash.com/random/384x288?software",
     },
     {
       title:
         "Business savvy tech experts helping forward thinking companies get things done.",
       link: "/",
-      img: "",
+      img: "https://source.unsplash.com/random/384x288?software",
     },
     {
       title:
         "Vancouver's tower crane rental service and support leader since 1974.",
       link: "/",
-      img: "",
+      img: "https://source.unsplash.com/random/384x288?software",
     },
   ],
 };
