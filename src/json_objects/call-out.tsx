14153bd54eb76bba1fc90142f6472fdbbd144774
usamakhangt4 <usamakhangt4@gmail.com>
2021-07-11 18:23:17 +0500

    completed testinmonials section

---
+++ src/json_objects/call-out.tsx
@@ -0,0 +1,6 @@
+export const callOutData = {
+  title: "Interested in collaborating or investing?",
+  description:
+    "I’m always open to discussing product design work or partnership opportunities.",
+  buttonText: "Start a conversation",
+};
---
+++ src/json_objects/testimonials.tsx
@@ -0,0 +1,30 @@
+export const testimonialsData = {
+  heading: "Testimonials",
+  description: "People I've worked with have said some nice things...",
+  testimonials: [
+    {
+      client_img: "/images/testimonials/pascal.png",
+      client_testimonial: (
+        <>
+          “Matt was a real pleasure to work with and we look forward to working
+          with him again. He’s definitely the kind of designer you can trust
+          with a project from start to finish.”
+        </>
+      ),
+      client_name: "Pascal Tremblay",
+      client_designation: "Creative Lead, Good Kind",
+    },
+    {
+      client_img: "/images/testimonials/alvin.png",
+      client_testimonial: (
+        <>
+          “Matt's a clear communicator with the tenacity and confidence to
+          really dig in to tricky design scenarios and the collaborative
+          friction that's needed to produce excellent work.”
+        </>
+      ),
+      client_name: "Alvin Engler",
+      client_designation: "Sr. Systems Developer, Domain7",
+    },
+  ],
+};
